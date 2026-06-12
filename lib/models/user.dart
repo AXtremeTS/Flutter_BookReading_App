b094316cc52972d2aedcfc3aa9b6edb726310db4
kietietv <vankiet01012005@gmail.com>
2026-06-12 22:11:10 +0700

    Change password to mutable and add isDeactivated

--- lib/models/user.dart
+++ lib/models/user.dart
@@ -1,11 +1,13 @@
 class User {
   final String username;
-  final String password;
+  String password;
   final bool isAdmin;
-
+  bool isDeactivated;
+  
   User({
     required this.username,
     required this.password,
     this.isAdmin = false,
+    this.isDeactivated = false,
   });
 }
